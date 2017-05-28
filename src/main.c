282b197fa6d2259b2e0b1c4cc7d476e272cfd961
a.vmarkov@gmail.com <a.vmarkov@gmail.com>
2017-05-28 23:21:35 +0400

    initial commit. Can execute input command

---
+++ src/main.c
@@ -0,0 +1,25 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <errno.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <errno.h> 
+#include <string.h>
+
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <linux/if_ether.h>
+#include <linux/filter.h>
+#include <pthread.h>
+
+int main(int argc, const char *argv[])
+{
+    printf("Hello world\n");
+}
---
+++ src/pgch.c
@@ -0,0 +1,54 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <errno.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <string.h>
+
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <linux/if_ether.h>
+#include <linux/filter.h>
+#include <pthread.h>
+
+#define BUFSIZE 1024
+
+int parse_output(char* cmd) {
+    char buf[BUFSIZE];
+    FILE *fp;
+
+    if ((fp = popen(cmd, "r")) == NULL) {
+        printf("Error opening pipe!\n");
+        return -1;
+    }
+
+    while (fgets(buf, BUFSIZE, fp) != NULL) {
+        // Do whatever you want here...
+        printf("OUTPUT: %s", buf);
+    }
+
+    if(pclose(fp))  {
+        printf("Command not found or exited with error status\n");
+        return -1;
+    }
+
+    return 0;
+}
+
+
+int main(void) {
+	puts("Hello World!");
+
+	parse_output("ls -la");
+	parse_output("/system/bin/input touchscreen swipe 300 300 500 1000 100");
+
+	return 0;
+}
